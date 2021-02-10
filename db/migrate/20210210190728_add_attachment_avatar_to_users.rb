f54ef40af80e5ba9d3625be8d3d77c9f930339f4
AndreaJasper <andrea.nicole.jasper@gmail.com>
2021-02-10 15:59:42 -0700

    Done.

---
+++ db/migrate/20210210190728_add_attachment_avatar_to_users.rb
@@ -0,0 +1,11 @@
+class AddAttachmentAvatarToUsers < ActiveRecord::Migration[5.1]
+  def self.up
+    change_table :songs do |t|
+      t.attachment :album_art
+    end
+  end
+
+  def self.down
+    remove_attachment :songs, :album_art
+  end
+end
